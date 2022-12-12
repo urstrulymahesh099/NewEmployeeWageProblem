044f28d337a1931b163c5f7c382957fecb74d150
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-12 16:13:14 +0530

    [Mahesh]Add:Check Employee Attendence Is Present Or Not

---
+++ NewEmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEmployeeWageProblem
+{
+    internal class EmployeeWage
+    {
+        public static void Attendence()
+        {
+            int IS_Full_Time = 1;
+            Random random = new Random();
+            int Emp=random.Next(0,2);
+            if(Emp== IS_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ NewEmployeeWageProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace NewEmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("WelCome To Employee Wage Problem");
+                Console.WriteLine("Please Click Below options: \n1.EmployeeAttendence");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        EmployeeWage.Attendence();
+                        break;
+                    default:
+                        Console.WriteLine("Please click error-free option");
+                        break;
+                }
+            }
+        }
+    }
+}
